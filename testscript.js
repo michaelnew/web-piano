730b7aae3671a720530b09c4f6bf583a5c06ceb3
Mike New <mikenew12@gmail.com>
2016-10-04 18:46:17 -0700

    added include folder with dependencies

--- testscript.js
+++ testscript.js
@@ -1,1 +1,17 @@
-alert("this script lives in a file")
+// alert("this script lives in a file");
+
+// simple example to get started;
+MIDI.loadPlugin({
+	soundfontUrl: "./include/soundfont/",
+    instrument: "acoustic_grand_piano", // or the instrument code 1 (aka the default)
+    // instruments: [ "acoustic_grand_piano", "acoustic_guitar_nylon" ], // or multiple instruments
+    onsuccess: function() { 
+    	var delay = 0; // play one note every quarter second
+			var note = 50; // the MIDI note
+			var velocity = 127; // how hard the note hits
+			// play the note
+			MIDI.setVolume(0, 127);
+			MIDI.noteOn(0, note, velocity, delay);
+			MIDI.noteOff(0, note, delay + 0.75);
+	}
+});
