6b50703f8487e4a2c4d23e9c67a9bd904d4e36e0
Mike New <mikenew12@gmail.com>
2016-10-04 20:00:33 -0700

    midi data is parse from uploaded file

--- testscript.js
+++ testscript.js
@@ -8,64 +8,73 @@ MIDI.loadPlugin({
     instrument: "acoustic_grand_piano", // or the instrument code 1 (aka the default)
     // instruments: [ "acoustic_grand_piano", "acoustic_guitar_nylon" ], // or multiple instruments
     onsuccess: function() { 
-  //   	var delay = 0; // play one note every quarter second
+  		// var delay = 0; // play one note every quarter second
 		// var note = 50; // the MIDI note
 		// var velocity = 127; // how hard the note hits
-			// // play the note
 		MIDI.setVolume(0, 127);
 		// MIDI.noteOn(0, note, velocity, delay);
 		// MIDI.noteOff(0, note, delay + 0.75);
 
 		var song = 'data:audio/mid;base64,TVRoZAAAAAYAAQABAMBNVHJrAAAARwD/WAQEAhgIAP9RAwehIAD/AwlOZXcgVHJhY2sAwHMAkDxkMoA8MIEOkDxkMoA8MIEOkDxkMoA8MIEOkDxkgT+APDAB/y8A';
-
-		// var title = document.getElementById("title");
-		// title.innerHTML = "Sound being generated with " + MIDI.api + " " + JSON.stringify(MIDI.supports);
-
-		/// this sets up the MIDI.Player and gets things going...
-		// player.timeWarp = 1; // speed the song is played back
 		player = MIDI.Player;
 		player.loadFile(song, player.start);
-
-		/// control the piano keys colors
-		// var colorMap = MIDI.Synesthesia.map();
-		// player.addListener(function(data) {
-		// 	var pianoKey = data.note - 21;
-		// 	var d = colorElements[pianoKey];
-		// 	if (d) {
-		// 		if (data.message === 144) {
-		// 			var map = colorMap[data.note - 27];
-		// 			if (map) d.style.background = map.hex;
-		// 			d.style.color = "#fff";
-		// 		} else {
-		// 			d.style.background = "";
-		// 			d.style.color = "";
-		// 		}
-		// 	}
-		// });
-		///
-		// ColorSphereBackground();
-		// MIDIPlayerPercentage(player);
 	}
 });
 
+function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
 
-function readSingleFile(e) {
-  var file = e.target.files[0];
-  if (!file) {
-    return;
+    // files is a FileList of File objects. List some properties.
+    var output = [];
+    for (var i = 0, f; f = files[i]; i++) {
+      output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+                  f.size, ' bytes, last modified: ',
+                  f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+                  '</li>');
+      var reader = new FileReader();
+
+      // Closure to capture the file information.
+      reader.onload = (function(theFile) {
+        return function(e) {
+          // Render thumbnail.
+          var span = document.createElement('span');
+          var m = MidiFile(e.target.result);
+          debugger;
+          span.innerHTML = m["data"];
+          // span.innerHTML = ['contents: ', e.target.result,
+          //                   '" title: "', escape(theFile.name), '"/>'].join('');
+          document.getElementById('list').insertBefore(span, null);
+
+        };
+      })(f);
+
+      reader.readAsBinaryString(f);
+      // reader.readAsText(f);
+    }
+    document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
   }
-  var reader = new FileReader();
-  reader.onload = function(e) {
-    var contents = e.target.result;
-    displayContents(contents);
-  };
-  reader.readAsText(file);
-}
 
-function displayContents(contents) {
-  var element = document.getElementById('file-content');
-  element.innerHTML = contents;
-}
+  document.getElementById('files').addEventListener('change', handleFileSelect, false);
 
-document.getElementById('file-input')
-  .addEventListener('change', readSingleFile, false);
+
+// function readSingleFile(e) {
+//   var file = e.target.files[0];
+//   if (!file) {
+//     return;
+//   }
+//   var reader = new FileReader();
+//   reader.onload = function(e) {
+//     var contents = e.target.result;
+//     displayContents(contents);
+//   };
+//   reader.readAsText(file);
+// }
+
+// function displayContents(contents) {
+//   var element = document.getElementById('file-content');
+//   element.innerHTML = contents;
+//   // MidiFile(contents);
+// }
+
+// document.getElementById('file-input')
+//   .addEventListener('change', readSingleFile, false);
