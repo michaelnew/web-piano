ad1090dcbfa2e6d5063d530aa15a11d857a28766
Mike New <mikenew12@gmail.com>
2017-01-24 15:38:14 -0800

    black keys show in the correct position and z sort

--- piano_visualizer.js
+++ piano_visualizer.js
@@ -6,13 +6,17 @@ const keyHeight = 130;
 const keyGap = 2;
 const keyCornerRadius = 9;
 
+const blackKeyWidth = 18;
+const blackKeyHeight = 80;
+const blackKeyCornerRadius = 6;
+
 const blackNotes = [22,25,27,30,32,34,37,39,42,44,46,49,51,54,56,58,61,63,66,68,70,73,75,78,80,82,85,87,90,92,94,97,99,102,104,106];
 
 function PianoVisualizer(stage) {
 	this.stage = stage;
 
 	this.keyMap = {};
-	this.generateKeys(45, 75);
+	this.generateKeys(45, 76);
 }
 
 PianoVisualizer.prototype.toggleKey = function(note, on) {
@@ -27,19 +31,33 @@ PianoVisualizer.prototype.toggleKey = function(note, on) {
 
 PianoVisualizer.prototype.generateKeys = function(start, end) {
 	var p = 0;
+	var blackKeys = [];
 	for (var i = start; i <= end; i++) {
 
 		var isBlack = blackNotes.indexOf(i) > -1;
 
+		if (isBlack) p--;
+
 		var k = new key(p, isBlack);
 		this.stage.addChild(k.keyShape);
+
+		if (isBlack) {
+			blackKeys.push(k);
+		}
 
 		this.keyMap[i] = k;
 
 		this.toggleKey(i, false);
-		if (!isBlack) p++;
+		p++;
 	}
+
 	console.log(this.keyMap);
+
+    for (var i = 0, k; k = blackKeys[i]; i++) {
+		console.log(k);
+		this.stage.setChildIndex(k.keyShape, this.stage.getNumChildren()-1);
+		this.stage.update();
+	}
 }
 
 
@@ -47,18 +65,25 @@ function key(position, isBlack) {
 	this.isBlack = isBlack;
 
 	this.keyShape = new createjs.Shape();
-	this.keyShape.x = xOffset + (position * (keyWidth + keyGap));
+	if (isBlack) {
+		let extraOffset = (keyWidth + keyGap * .5) - blackKeyWidth * .5;
+		this.keyShape.x = xOffset + (position * (keyWidth + keyGap)) + extraOffset;
+	} else {
+		this.keyShape.x = xOffset + (position * (keyWidth + keyGap));
+	}
 	this.keyShape.y = yOffset;
 }
 
 key.prototype.toggle = function(on) {
-	// use #55573 for black keys
+
 	const r = keyCornerRadius;
+	const br = blackKeyCornerRadius;
+
 	if (this.isBlack) {
 		if (on) {
-			this.keyShape.graphics.clear().beginStroke("#282").beginFill("#3BBFC0").drawRoundRectComplex(0, 0, keyWidth*.6, keyHeight*.6, 0,0,r,r).endFill().endStroke();
+			this.keyShape.graphics.clear().beginStroke("#282").beginFill("#3BBFC0").drawRoundRectComplex(0, 0, blackKeyWidth, blackKeyHeight, 0,0,br,br).endFill().endStroke();
 		} else {
-			this.keyShape.graphics.clear().beginStroke("#222").beginFill("#4B4D49").drawRoundRectComplex(0, 0, keyWidth*.6, keyHeight*.6, 0,0,r,r).endFill().endStroke();
+			this.keyShape.graphics.clear().beginStroke("#222").beginFill("#4B4D49").drawRoundRectComplex(0, 0, blackKeyWidth, blackKeyHeight, 0,0,br,br).endFill().endStroke();
 		}
 	} else {
 		if (on) {
