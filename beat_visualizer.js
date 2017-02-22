79102284e4782a8af0ecd73ad19062ff7fa0b630
Mike New <mikenew12@gmail.com>
2017-02-21 18:01:54 -0800

    beat channels now have subdivision changing buttons

--- beat_visualizer.js
+++ beat_visualizer.js
@@ -67,14 +67,18 @@ function BeatChannel(x, triggerPointY, note, triggerCallback, stage) {
 
 	this.container = new createjs.Container();
 	this.container.x = x;
+	this.container.tickChildren = true;
+	this.container.mouseEnabled = true;
+	this.container.mouseChildren = true;
 
 	this.marker = new createjs.Shape();
 
 	let hitArea = new createjs.Shape();
 	let w = nodeRadius * 2 + beatNodeStrikeWidth;
 	let h = 500;
-	hitArea.graphics.beginFill("#fff").drawRect(- w * .5, 0, w, h);
-	this.container.hitArea = hitArea;
+	hitArea.graphics.beginFill("#222").drawRect(- w * .5, 0, w, h);
+	//hitArea.alpha = .01;
+	//this.container.hitArea = hitArea;
 	this.container.addChild(hitArea);
 
 	this.moveCallback;
@@ -86,23 +90,70 @@ function BeatChannel(x, triggerPointY, note, triggerCallback, stage) {
 	this.shape.y = + pixelsPerBeat * triggerBeat;
 	this.shape.graphics.clear().setStrokeStyle(beatNodeStrikeWidth).beginStroke(NODE_STROKE_DULL).drawCircle(0, 0, nodeRadius + beatNodeStrikeWidth).endStroke();
 
-	this.label = new createjs.Text("4", "100 16px Roboto", "#D2CFCE");
-	this.label.x = w; 
-	this.label.y = this.shape.y;
-	this.label.textBaseline = "middle";
+	this.tempoUp = new createjs.Shape();
+	this.tempoDown = new createjs.Shape();
+	this.createTempoButtons(this.shape.y);
 
 	let c = this;
 	this.container.on("pressmove", function(evt) {
-		evt.target.x = evt.stageX / stage.scaleX;
+		if (evt.target == hitArea) {
+			c.container.x = evt.stageX / stage.scaleX;
+		}
 	});
 
 	this.container.on("pressup", function(evt) {
-		c.snapToNearestPoint();
+		if (evt.target == hitArea) {
+			c.snapToNearestPoint();
+		}
 	});
 
 	this.container.addChild(this.marker);
 	this.container.addChild(this.shape);
 	this.container.addChild(this.label);
+	this.container.addChild(this.tempoUp);
+	this.container.addChild(this.tempoDown);
+}
+
+BeatChannel.prototype.createTempoButtons = function(y) {
+
+	let x = nodeRadius * 2 + beatNodeStrikeWidth;
+	let sx = 8;
+	let sy = 4;
+	let dy = 14;
+
+	this.tempoUp.graphics.clear().beginStroke(COLOR_6)			
+		.moveTo(x - sx * .5, y + dy)
+		.lineTo(x, y + sy + dy)
+		.lineTo(x + sx * .5, y + dy)
+
+	let hitArea = new createjs.Shape();
+	hitArea.graphics.beginFill("#fff").drawRect(x - 10, y - 10 - dy, 20, 20);
+	this.tempoUp.hitArea = hitArea;
+	//this.container.addChild(hitArea);
+
+	let t = this;
+	this.tempoUp.on("pressup", function(evt) {
+		t.resetToSubdivisions(t.label.text + 1);
+	});
+
+	this.tempoDown.graphics.clear().beginStroke(COLOR_6)			
+		.moveTo(x - sx * .5, y - dy)
+		.lineTo(x, y - sy - dy)
+		.lineTo(x + sx * .5, y - dy)
+
+	let hitArea2 = new createjs.Shape();
+	hitArea2.graphics.beginFill("#ff0").drawRect(x - 10, y - 10 + dy, 20, 20);
+	this.tempoDown.hitArea = hitArea2;
+	//this.container.addChild(hitArea2);
+
+	this.tempoDown.on("pressup", function(evt) {
+		t.resetToSubdivisions(t.label.text - 1);
+	});
+
+	this.label = new createjs.Text("4", "100 16px Roboto", "#D2CFCE");
+	this.label.x = x - 4; 
+	this.label.y = y;
+	this.label.textBaseline = "middle";
 }
 
 BeatChannel.prototype.snapToNearestPoint = function() {
@@ -130,13 +181,12 @@ BeatChannel.prototype.getCenterX = function() {
 	return this.container.x + beatLineWidth * .5;
 }
 
-
 BeatChannel.prototype.addSubdividedNodes = function(subdivisions) {
 	this.label.text = subdivisions;
 	for (let b = 0; b < beatsPerNode; b++) {
 		for (let i = 0; i < subdivisions; i++) {
 
-			let node = new BeatNode(0);
+			let node = new BeatNode();
 			node.startBeat = i/subdivisions + b;
 			node.endBeat = 3 + i/subdivisions + b;
 			node.repeat = true;
@@ -148,6 +198,17 @@ BeatChannel.prototype.addSubdividedNodes = function(subdivisions) {
 			this.container.addChild(node.shape);
 		}
 	}
+}
+
+BeatChannel.prototype.resetToSubdivisions = function(subdivisions) {
+
+    for (let i = 0, n; n = this.nodes[i]; i++) {
+		this.container.removeChild(n.shape);
+	}
+
+	this.nodes = [];
+
+	this.addSubdividedNodes(subdivisions);
 }
 
 BeatChannel.prototype.tick = function(time) {
@@ -240,7 +301,7 @@ BeatChannel.prototype.triggerNearestNodeAtTime = function(time) {
 
 BeatChannel.prototype.addNodeAtTime = function(time) {
 
-	let node = new BeatNode(0);
+	let node = new BeatNode();
 	node.startBeat = time - triggerBeat;
 	node.endBeat = time + triggerBeat;
 	node.normalColor = BEAT_NODE_USER;
@@ -257,6 +318,5 @@ function BeatNode() {
 	this.triggered = false;
 	this.repeat = false;
 
-	this.shape.y = nodeRadius;
 	this.shape.graphics.clear().beginFill(BEAT_NODE).drawCircle(0, 0, nodeRadius).endFill();
 }
